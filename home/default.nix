9638a26720b9cafb229fb4bf3c1c56b856959587
Mega4349 <elliot.mortberg@gmail.com>
2023-12-22 23:30:11 +0100

    restructure

--- home/default.nix
+++ home/default.nix
@@ -2,30 +2,29 @@
 
 {
   imports = [
-    inputs.impermanence.nixosModules.home-manager.impermanence
+		inputs.impermanence.nixosModules.home-manager.impermanence
 		../pkgs
 		./modules/wayland
-		#./modules/config
-    ./modules/firefox.nix
-    ./modules/filemanagers.nix
-    ./modules/filemanagers.nix
-    ./modules/foot.nix
-    ./modules/discord.nix
-    ./modules/games.nix #add aagl
+		./modules/firefox.nix
+		./modules/filemanagers.nix
+		./modules/filemanagers.nix
+		./modules/foot.nix
+		./modules/discord.nix
+		./modules/games.nix #add aagl
 		./modules/git.nix
-    ./modules/theme.nix
+		./modules/theme.nix
 		./modules/mpv.nix
-    ./modules/sound.nix
+		./modules/sound.nix
 		./modules/nix.nix
-    ./modules/neovim.nix
+		./modules/neovim.nix
 		#./modules/spotify.nix
-    ./modules/obs.nix
+		./modules/obs.nix
 		./modules/tmux.nix
 		./modules/utilities.nix
 		./modules/virtualisation.nix
-    #./modules/wezterm.nix
-    ./modules/shell.nix
-    ./modules/xdg.nix
+		#./modules/wezterm.nix
+		./modules/shell.nix
+		./modules/xdg.nix
   ];
 
   programs.home-manager.enable = true;
