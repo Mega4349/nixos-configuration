404bb6bfb67114196de8380bc5e9833ea8570503
Mega4349 <elliot.mortberg@gmail.com>
2024-01-20 15:24:19 +0100

    add gnome

--- modules/home.nix
+++ modules/home.nix
@@ -12,11 +12,32 @@
     users.mega = import ../home;
   };
 
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
+  environment = {
+    sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+    };
+    gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+    gedit # text editor
+  ]) ++ (with pkgs.gnome; [
+    cheese # webcam tool
+    gnome-music
+    gnome-terminal
+    epiphany # web browser
+    geary # email reader
+    evince # document viewer
+    gnome-characters
+    totem # video player
+    tali # poker game
+    iagno # go game
+    hitori # sudoku game
+    atomix # puzzle game
+  ]);
   };
   
   programs = {
+    sway.enable = true;
     dconf.enable = true; # needed to make home-manager happy
     fuse.userAllowOther = true;
     # program to show keypresses in wayland, needs setuid binary which is provided by the nixos module
@@ -34,6 +55,7 @@
   };
   
   services = {
+    xserver.desktopManager.gnome.enable = true;
     gvfs.enable = true; # For mounting without root and trash 
 	  tumbler.enable = true; # Thumbnail support for images
 	  dbus.enable = true;
