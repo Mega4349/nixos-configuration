4a17f952ad4d9052eaa29064f7113a9b4809429b
Mega4349 <elliot.mortberg@gmail.com>
2024-02-01 20:35:40 +0100

    remove unused fonts, cleanup

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -7,17 +7,19 @@
       noto-fonts
       noto-fonts-cjk
       noto-fonts-emoji
+
       font-awesome
+      
       source-han-sans
       source-han-sans-japanese
       source-han-serif-japanese
+      
       ubuntu_font_family
+      
       (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
       jetbrains-mono
+      
 			maple-mono
-      #nerdfonts
-      #roboto-mono
-			fira
     ];
     fontconfig.defaultFonts = {
       monospace = [ "Maple Mono" ];
