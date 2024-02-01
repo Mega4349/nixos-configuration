33dad1e6a475f6e9bb3ee7bbff0d7a941b5195ed
Mega4349 <elliot.mortberg@gmail.com>
2024-02-01 20:16:03 +0100

    change some fusemounts to symlinks in home directory

--- home/general/persist.nix
+++ home/general/persist.nix
@@ -4,14 +4,14 @@
   home.persistence."/nix/persist/home/mega" = {
     allowOther = true;
     directories = [
-      "Desktop"
-      "Downloads"
-      "Pictures"
-      "Music"
-      "Videos"
-      "Documents"
-      "VMs"
-      "Projects"
+      { directory = "Desktop"; method = "symlink"; }
+      { directory = "Downloads"; method = "symlink"; }
+      { directory = "Pictures"; method = "symlink"; }
+      { directory = "Music"; method = "symlink"; }
+      { directory = "Videos"; method = "symlink"; }
+      { directory = "Documents"; method = "symlink"; }
+      { directory = "VMs"; method = "symlink"; }
+      { directory = "Projects"; method = "symlink"; }
 
       ".cache"
 
