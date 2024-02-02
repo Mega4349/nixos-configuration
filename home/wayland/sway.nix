b379d05cc3d1d620f567bcd4bd304f7f7e386a8c
Mega4349 <elliot.mortberg@gmail.com>
2024-02-02 20:34:35 +0100

    make obs-studio be a floating window by default

--- home/wayland/sway.nix
+++ home/wayland/sway.nix
@@ -219,6 +219,8 @@ in
       # set floating
       for_window [app_id="float"] floating enable
 
+      for_window [app_id="com.obsproject.Studio"] floating enable
+
       for_window [app_id="blueman-manager"] floating enable, resize set width 40 ppt height 30 ppt
       for_window [app_id="OpenTabletDriver.UX.Gtk"] floating enable, resize set width 60 ppt height 75 ppt
       for_window [app_id="pavucontrol" ] floating enable, resize set width 40 ppt height 30 ppt
