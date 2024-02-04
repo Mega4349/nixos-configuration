56aedb5b201dc151c9628b468acce9c0b50cfd4f
Mega4349 <elliot.mortberg@gmail.com>
2024-02-04 19:54:35 +0100

    update mpv configuration

--- home/gui/mpv.nix
+++ home/gui/mpv.nix
@@ -7,34 +7,42 @@
 
 	programs.mpv = {
     enable = true;
-    #scripts = with pkgs.mpvScripts; [
-    #  sponsorblock
-    #  webtorrent-mpv-hook
-    #  quality-menu
-    #  thumbfast
-    #];
+    scripts = with pkgs.mpvScripts; [
+      sponsorblock
+      thumbfast
+			mpv-playlistmanager
+			mpv-cheatsheet
+			mpris
+    ];
     config = {
-      profile = "gpu-hq";
       volume = "80";
       hwdec = "auto";
-      #gpu-api = "vulkan";mis
+      gpu-api = "vulkan";
       gpu-context = "wayland";
       vo = "gpu-next,gpu,dmabuf-wayland";
-      ao = "pipewire"; #,pulse";
-      scale = "ewa_lanczos";
+      ao = "pipewire";
+			deband = "yes";
+      scale = "ewa_lanczossharp";
       dscale = "ewa_robidoux";
       cscale = "mitchell";
       dither-depth = "auto";
       dither = "fruit";
-      vd-lavc-film-grain = "gpu";
-      #vd-lavc-av1-gpuhybrid = "gpu";
-      #vd-lavc-maxframedelay = "16";
+      vd-lavc-film-grain = "auto";
       tone-mapping = "bt.2446a";
       tone-mapping-mode = "hybrid";
       gamut-mapping-mode = "clip";
       target-trc = "bt.1886";
       alang = "jpn,jp,eng,en,enUS,en-US";
+			force-window = "immediate";
+			term-osd-bar = "yes";
+			screenshot-format = "png";
       screenshot-directory = "~/Pictures/mpv";
+			screenshot-png-compression = "7";
     };
   };
+
+	xdg.configFile = {
+		"mpv/scripts".source = ./config/mpv/scripts;
+		"mpv/script-opts".source = ./config/mpv/script-opts;
+	};
 }
