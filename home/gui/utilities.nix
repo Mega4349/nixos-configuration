d345bca423604879e3a449a939a1c4f6d02f0b86
Mega4349 <elliot.mortberg@gmail.com>
2024-02-24 21:03:19 +0100

    avidemux issue closed, it likely works without the overlay

--- home/gui/utilities.nix
+++ home/gui/utilities.nix
@@ -1,23 +1,6 @@
 { pkgs, inputs, ... }:
 
 {
-	nixpkgs.overlays = [
-		#https://github.com/NixOS/nixpkgs/issues/239424
-    (final: prev: {
-      avidemux = prev.avidemux.overrideAttrs (oldAttrs: {
-        buildCommand = oldAttrs.buildCommand + ''
-          mv $out/bin/avidemux $out/bin/avidemux3
-          echo 'LD_LIBRARY_PATH=$(dirname $(readlink -f $(which avidemux3)))/../lib/ avidemux3 "$@"' >> $out/bin/avidemux
-          chmod +x $out/bin/avidemux
-
-          mv $out/bin/avidemux3_cli $out/bin/avidemux3_cli_old
-          echo 'LD_LIBRARY_PATH=$(dirname $(readlink -f $(which avidemux3_cli)))/../lib/ avidemux3_cli_old "$@"' >> $out/bin/avidemux3_cli
-          chmod +x $out/bin/avidemux3_cli
-        '';
-      });
-    })
-  ];
-
 	home = {
 		packages = with pkgs; [
 			krita
