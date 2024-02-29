0f7e4503c445836223cf9e35f54eea725d459096
Mega4349 <elliot.mortberg@gmail.com>
2024-02-29 19:48:03 +0100

    move some things from nix.nix to system.nix

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,31 +1,23 @@
-{ inputs, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  nix = {
-    settings = {
-      experimental-features = [ "nix-command" "flakes" ];
-      trusted-users = [ "root" "mega" "@wheel" ]; # Trusted users can use binary cache
-      auto-optimise-store = true; # Optimizing /nix/store 
-      connect-timeout = 40000; # Increased timeout because my internet is bad sometimes
-			substituters = [
-      	"https://nix-community.cachix.org/"
-      	"https://nix-gaming.cachix.org/"
-      	"https://anyrun.cachix.org/"
-      	"https://ezkea.cachix.org"
-    	];
-    	trusted-public-keys = [
-      	"nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      	"nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      	"anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
-      	"ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
-    	];
-    };
-    # Automatic garbage collection
-    gc = { 
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    trusted-users = [ "root" "mega" "@wheel" ]; # Trusted users can use binary cache
+    auto-optimise-store = true; # Optimizing /nix/store 
+    connect-timeout = 40000; # Increased timeout because my internet is bad sometimes
+  	substituters = [
+    	"https://nix-community.cachix.org/"
+    	"https://nix-gaming.cachix.org/"
+    	"https://anyrun.cachix.org/"
+    	"https://ezkea.cachix.org"
+  	];
+  	trusted-public-keys = [
+    	"nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    	"nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+    	"anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+    	"ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
+  	];
   };
 
   nixpkgs.config = {
@@ -36,12 +28,4 @@
   environment.systemPackages = with pkgs; [
     cachix
   ];
-
-  time.timeZone = "Europe/Stockholm";
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  system.stateVersion = "23.05";
-
-  environment.etc."programs.sqlite".source = inputs.programsdb.packages.${pkgs.system}.programs-sqlite;
-  programs.command-not-found.dbPath = "/etc/programs.sqlite";
 }
---
+++ modules/system.nix
@@ -0,0 +1,19 @@
+{ inputs, ... }:
+
+{
+  nix.gc = { 
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
+
+  time.timeZone = "Europe/Stockholm";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  system.stateVersion = "23.05";
+
+  environment.etc."programs.sqlite".source = inputs.programsdb.packages.${pkgs.system}.programs-sqlite;
+  
+  programs.command-not-found.dbPath = "/etc/programs.sqlite";
+}
