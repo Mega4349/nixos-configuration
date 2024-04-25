c24c4b566666841b9d08598875ac8a2cf08bc322
Mega4349 <elliot.mortberg@gmail.com>
2024-04-25 15:40:52 +0200

    add vr resources

--- modules/vr.nix
+++ modules/vr.nix
@@ -1,17 +1,15 @@
 { lib, config, pkgs, ... }:
 
 {
-  lib.mkIf(config.networking.hostName == "nixos-desktop") {
-    services.monado = {
-      enable = true;
-      defaultRuntime = true; # Register as default OpenXR runtime
-    };
-    systemd.user.services.monado.environment = {
-      STEAMVR_LH_ENABLE = "1";
-      XRT_COMPOSITOR_COMPUTE = "1";
-    };
-    environment.systemPackages = with pkgs; [
-      opencomposite
-    ];
+  services.monado = lib.mkIf (config.networking.hostName == "nixos-desktop") {
+    enable = true;
+    defaultRuntime = true; # Register as default OpenXR runtime
   };
+  systemd.user.services.monado.environment = lib.mkIf (config.networking.hostName == "nixos-desktop") {
+    STEAMVR_LH_ENABLE = "1";
+    XRT_COMPOSITOR_COMPUTE = "1";
+  };
+  environment.systemPackages = with pkgs; lib.mkIf (config.networking.hostName == "nixos-desktop") [
+    opencomposite
+  ];
 }
