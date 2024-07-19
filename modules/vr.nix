e3eefd07ce87c9ead2c011a5d511a49f9f21c7d2
Mega4349 <elliot.mortberg@gmail.com>
2024-07-19 22:21:09 +0200

    stop a bit of vr stuff from building on laptop

--- modules/vr.nix
+++ modules/vr.nix
@@ -1,20 +1,20 @@
 { lib, config, pkgs, ... }:
 
 {
-  programs = {
+  programs = lib.mkIf (config.networking.hostName == "nixos-desktop") {
     envision.enable = true;
     corectrl.enable = true;
   };
-  services = {
+  services = lib.mkIf (config.networking.hostName == "nixos-desktop") {
     udev.packages = with pkgs; [
       xr-hardware
     ];
-    monado = lib.mkIf (config.networking.hostName == "nixos-desktop") {
+    monado = {
       enable = true;
       defaultRuntime = true; # Register as default OpenXR runtime
     };
   };
-  systemd.user.services.monado.environment = lib.mkIf (config.networking.hostName == "nixos-desktop") {
+  systemd.user.services.monado.environment = {
     STEAMVR_LH_ENABLE = "1";
     XRT_COMPOSITOR_COMPUTE = "1";
   };
