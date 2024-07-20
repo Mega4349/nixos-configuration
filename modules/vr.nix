48f3ff076c87a7805fbffb024697353c28a6dba5
Mega4349 <elliot.mortberg@gmail.com>
2024-07-20 18:17:34 +0200

    remove nixpkgs monado-service, envision builds own monado

--- modules/vr.nix
+++ modules/vr.nix
@@ -5,16 +5,15 @@ lib.mkIf (config.networking.hostName == "nixos-desktop") {
     envision.enable = true;
     corectrl.enable = true;
   };
-  services.monado = {
-      enable = true;
-      defaultRuntime = true; # Register as default OpenXR runtime
-    };
-  systemd.user.services.monado.environment = {
-    STEAMVR_LH_ENABLE = "1";
-    XRT_COMPOSITOR_COMPUTE = "1";
-  };
-  environment.systemPackages = with pkgs; [
-    opencomposite
-    usbutils
-  ];
+  #services.monado = {
+  #    enable = true;
+  #    defaultRuntime = true; # Register as default OpenXR runtime
+  #  };
+  #systemd.user.services.monado.environment = {
+  #  STEAMVR_LH_ENABLE = "1";
+  #  XRT_COMPOSITOR_COMPUTE = "1";
+  #};
+  #environment.systemPackages = with pkgs; [
+  #  opencomposite
+  #];
 }
