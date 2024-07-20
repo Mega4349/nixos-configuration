b14b96a7dd96c77d9a6bf8de0bbbd278639c8709
Mega4349 <elliot.mortberg@gmail.com>
2024-07-20 13:03:36 +0200

    update gtk theme

--- home/general/theme.nix
+++ home/general/theme.nix
@@ -4,7 +4,7 @@
   gtk = {
     enable = true;
     theme = {
-      name = "Tokyonight-Dark-BL";
+      name = "Tokyonight-Dark";
       package = pkgs.tokyonight-gtk-theme;
     };
     iconTheme = {
