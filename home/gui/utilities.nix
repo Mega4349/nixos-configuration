dd7afaebadc0b9a206739a4a0508d999fd1921a2
Mega4349 <elliot.mortberg@gmail.com>
2024-07-21 11:59:04 +0200

    split home utilities into general and desktop files

--- home/gui/utilities.nix
+++ home/gui/utilities.nix
@@ -24,19 +24,10 @@
 	    unzip
 		  xarchiver
 		];  
-		lib.mkIf (osConfig.networking.hostName == "nixos-desktop") packages = with pkgs; [
-			blender-hip
-			orca-slicer
-			wootility
-			via
-			avidemux
-		];
 
   	persistence."/nix/persist/home/mega" = {
 			directories = [
-				".config/blender"
 				".config/deluge"
-				".config/wootility-lekker"
 			];
 			files = [ # fricking kde apps and not having config directories
 				".config/kriarc"
