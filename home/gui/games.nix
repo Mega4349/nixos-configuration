3aea3b105d448a42d467b3f29994e01df420711c
Mega4349 <elliot.mortberg@gmail.com>
2024-07-21 11:57:24 +0200

    remove lazer vulkan script

--- home/gui/games.nix
+++ home/gui/games.nix
@@ -1,34 +1,17 @@
 { pkgs, inputs, ... }:
 
 let
-  lazer-vulkan = pkgs.writeTextFile {
-    name = "lazer-vulkan";
-    destination = "/bin/lazer-vulkan";
-    executable = true;
-
-    text = ''
-      #!/usr/bin/env bash
-      sed -i 's/Renderer = .*/Renderer = Vulkan/' $HOME/.local/share/osu/framework.ini
-      osu-lazer
-    '';
-  };
-
   games = inputs.nix-gaming.packages.${"x86_64-linux"};
 in
 {
   home = {
     packages = with pkgs; [
-      gnused
-
-      wlx-overlay-s
-
       prismlauncher
       jdk8
   	  heroic
       bottles
       steamtinkerlaunch
 
-      lazer-vulkan
   	  games.osu-lazer-bin
     ];
 
