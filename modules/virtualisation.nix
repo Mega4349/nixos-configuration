ddac46b9d5e826de84b64b84e3d0faf2e541a278
Mega4349 <elliot.mortberg@gmail.com>
2024-08-02 17:10:52 +0200

    add waydroid virtualisation

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -1,12 +1,18 @@
 { lib, pkgs, config, ... }:
 
-lib.mkIf (config.networking.hostName == "nixos-desktop") {
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu.verbatimConfig = ''
-      user = "root"
-      group = "root"
-      remember_owner = 0
-    '';
+{
+  virtualisation = {
+    waydroid.enable = true;
+    libvirtd = lib.mkIf (config.networking.hostName == "nixos-desktop") {
+      enable = true;
+      qemu.verbatimConfig = ''
+        user = "root"
+        group = "root"
+        remember_owner = 0
+      '';
+    };
   };
+  environment.systemPackages = with pkgs; [
+    lzip
+  ];
 }
